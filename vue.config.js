62607316589f37d06c085459aa4153f09ef87899
6238729 <6238729@gmail.com>
2021-10-07 11:42:53 +0900

    vue config 설정

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
-const { NODE_ENV, BASE_URL = '/' } = process.env
-
 module.exports = {
-  publicPath: NODE_ENV === 'production' ? BASE_URL : '/'
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/blog/'
+    : '/',
+    outputDir: 'docs'
 }
